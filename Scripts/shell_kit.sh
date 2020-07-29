96bb85654880568e957a116a1890a127a05191af
dafu <humoufeng@gmail.com>
2020-07-29 10:27:55 +0800

    shell: remove application path alias

--- Scripts/shell_kit.sh
+++ Scripts/shell_kit.sh
@@ -4,7 +4,6 @@ alias v="vim"
 alias ll="ls -l"
 alias setproxy="export ALL_PROXY=socks5://127.0.0.1:1080" 
 alias unsetproxy="unset ALL_PROXY"
-alias postman='/home/dafu/Applications/Postman/Postman &'
 alias shdn="sudo shutdown -h now"
 
 
